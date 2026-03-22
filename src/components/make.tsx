e74cdfcd75c8422d5c0332ee9f0c87d3ffc0a71b
Ayumu Fujimoto <fujimotoayumu0414@icloud.com>
2026-03-22 17:28:17 +0900

    数値から判別する関数の作成

--- src/components/make.tsx
+++ src/components/make.tsx
@@ -1,19 +1,43 @@
 import { useState } from 'react'
 
 type Schedule = {
+    name: string,
     date1: number,
     date2: number,
     date3: number
 }
 
-const schedule1: Schedule = {
-    date1: 1,
-    date2: 0,
-    date3: 2
-}
+
 
 export default function Make(){
     const [count, setCount] = useState<number>(0);
+    const [schedule1, setSchedule] = useState<Schedule>({
+        name: 'happy',
+        date1: 1,
+        date2: 0,
+        date3: 2
+    })
+
+    const removeSchedule = () =>{
+        setSchedule({
+            name: '',
+            date1: 0,
+            date2: 0,
+            date3: 0
+        })
+    }
+
+    const judgeSchedule = (d: number): string =>{
+        let j = ''
+        
+        if(d === 0) j = '×'
+        else if(d === 1) j = '◯'
+        else j = '△'
+
+
+        return j
+    }
+
 
     const addOne = () => {
         setCount(count + 1)
@@ -23,8 +47,17 @@ export default function Make(){
         <div>
             <p>{count}</p>
             <button onClick={addOne}>+1</button>
-            <p>スケジュール1:{schedule1.date1}
-            </p>
+
+            
+            <button onClick={removeSchedule}>削除</button>
+            <p>名前：{schedule1.name}</p>
+            <p>スケジュール1：{schedule1.date1}</p>
+            <p>スケジュール2：{schedule1.date2}</p>
+            <p>スケジュール3：{schedule1.date3}</p>
+            <p>判定</p>
+            <p>スケジュール1：{judgeSchedule(schedule1.date1)}</p>
+            <p>スケジュール2：{judgeSchedule(schedule1.date2)}</p>
+            <p>スケジュール3：{judgeSchedule(schedule1.date3)}</p>
         </div>
     )
 }
