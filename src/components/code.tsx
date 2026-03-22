efa0d0d7633134026fabaf67fa7b508f1225f2ae
akasuke26 <chiyanjunjie@gmail.com>
2026-03-22 19:16:26 +0900

    3/22 19:16

---
+++ src/components/code.tsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from 'react'
+
+function Code(){
+    //初期メッセージの設定
+    const [message,setMessage]=useState("こんにちは");
+    const dateTime=new Date();
+
+    useEffect(()=>{
+        if(dateTime.getHours()>=5 && dateTime.getHours()<10){
+            setMessage((message)=>"おはようございます");
+        }
+        else if(dateTime.getHours()>=17){
+            setMessage((message)=>"こんばんは");
+        }
+    },[message]);
+
+    return (
+        <div>
+            <h1>タイトル</h1>
+            <h2>{message}</h2>
+            <input type="text" placeholder="検索"></input>
+            <select name="sort">
+                <option>新しい順</option>
+                <option>お気に入り</option>
+                <option>古い順</option>
+            </select>
+            <button>+新規作成</button>
+        </div>
+    )
+}
+
+export default Code;
+
+// const Timer=()=>{
+//     //状態管理
+//     const [time, setTime]=useState(0);
+//     const [isRunning, setIsRunning]=useState(false);
+
+//     useEffect(()=>{
+//         if(isRunning){
+//             const interval=setInterval(()=>{
+//                 setTime((prevTime)=>prevTime+1);
+//             },1000);
+
+//             return ()=>clearInterval(interval);
+//         }
+//     },[isRunning]);
+
+//     const startTimer=()=>setIsRunning(true);
+//     const stopTimer=()=>setIsRunning(false);
+//     const resetTimer=()=>{
+//         setIsRunning(false);
+//         setTime(0);
+//     };
+
+//     return (
+//         <div>
+//             <h1>{time}秒</h1>
+//             <button onClick={startTimer} disabled={isRunning}>
+//                 開始
+//             </button>
+//             <button onClick={stopTimer} disabled={!isRunning}>
+//                 停止
+//             </button>
+//             <button onClick={resetTimer}>リセット</button>
+//         </div>
+//     );
+// };
+// export default Timer;
+
+// const RealtimeClock=()=>{
+//     const dateTime=new Date();
+//     const time=dateTime.getHours()+":"+dateTime.getMinutes()+":"+dateTime.getSeconds();
+
+//     return(
+//         <div>
+//             <p>{time}</p>
+//         </div>
+//     )
+// }
+
+// export default RealtimeClock;
