20c7b171994a9e12319ed231a4d0c8e3db5fb86c
Ayumu Fujimoto <fujimotoayumu0414@icloud.com>
2026-03-27 15:02:04 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import './App.css'
 import Make from './components/make.tsx'
 import Code from './components/code.tsx'
+import Home from './components/home.tsx'
+import { useState } from 'react'
+
 
 function App() {
+  const [page, setPage] = useState<'home' | 'make' | 'code'>('home')
+
+  let screen
+
+  if (page === 'home'){
+    screen = <Home />
+  }else if(page === 'code'){
+    screen = <Code />
+  }else if(page === 'make'){
+    screen = <Make />
+  }
+
   return (
     <>
-      <Code />
-      <Make />
+    
+      <div className="topBtns">
+        <button onClick={() => setPage('home')}>🏠</button>
+        <button onClick={() => setPage('make')}>日程調整へ</button>
+        <button onClick={() => setPage('code')}>テストページへ</button>
+      </div>
+
+      <div className="mainContent">
+        {screen}
+      </div>
+      
+    
     </>
   )
 }
